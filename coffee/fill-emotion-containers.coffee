453ec9a27dcebf68a747fb3ac82954b15bcd2abd
Robin Tibor Schirrmeister <robintibor@gmail.com>
2012-12-16 16:27:23 +0100

    first fully functionalv ersion with your emotional state also submitted to an aggreagtion database of everybodys emotional state

--- coffee/fill-emotion-containers.coffee
+++ coffee/fill-emotion-containers.coffee
@@ -1,49 +1,121 @@
 console.log($('.emotionContainer'))
 
 fillAllEmotionContainers = ->
+    fillInvididualEmotionContainers()
+    fillCommunityEmotionContainers()
+
+fillInvididualEmotionContainers = ->
     emotionContainerHTML = 
         "<div class='oneEmotion'>
-            <div class='emotionTexts'>
-                <span class='negative'>bored</span>
-                <span class='positive'>fascinated</span>
+            <div class='emotionPictures'>
+                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/frustrated.jpg' alt='frustated' class='firstEmotion' />
+                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/unchallenged.jpg' alt='unchallenged' class='secondEmotion' />
             </div>
         <div class='emotionSlider'></div>
         </div>
         <div class='oneEmotion'>
-            <div class='emotionTexts'>
-                <span class='negative'>overwhelmed</span>
-                <span class='positive'>unchallenged</span>
+            <div class='emotionPictures'>
+                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/bored.jpg' alt='bored' class='firstEmotion' />
+                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/excited.jpg' alt='excited' class='secondEmotion' />
             </div>
             <div class='emotionSlider'></div>
         </div>
         "
     $('.emotionContainer').html(emotionContainerHTML)
 
+fillCommunityEmotionContainers = ->
+    emotionContainerHTML = 
+        "<div class='communityEmotion'>
+            <div class='emotionPictures'>
+                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/frustrated.jpg' alt='frustated' class='firstEmotion' />
+                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/unchallenged.jpg' alt='unchallenged' class='secondEmotion' />
+            </div>
+        </div>
+        <div class='communityEmotion'>
+            <div class='emotionPictures'>
+                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/bored.jpg' alt='bored' class='firstEmotion' />
+                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/excited.jpg' alt='excited' class='secondEmotion' />
+            </div>
+        </div>
+        "
+    $('.communityEmotionContainer').html(emotionContainerHTML)
+
 addSlidersToEmotionContainers = ->
     $('.emotionSlider').slider({
-        min: 8,
-        max: 28
+        min: 0,
+        max: 10
         })
 
-setupSlidersToChangeTextOnChange = ->
+setupSlidersToChangePicturesOnChange = ->
     $( ".emotionSlider" ).on( "slide slidechange", (event, ui) ->
-        emotionTextElements = getTwoCorrespondingTextsOfSlider(ui.handle)
-        console.log("emotext", emotionTextElements)
-        setSizeOfEmotionTexts(emotionTextElements, ui.value)
-        console.log("slider changed!:) to #{ui.value}")
-        console.log(ui.handle)
+        emotionPictures = getTwoCorrespondingPicturesOfSlider(ui.handle)
+        setSizeOfEmotionPictures(emotionPictures, ui.value)
     )
-    $('.emotionSlider').slider("option", "value", 18);
+    $('.emotionSlider').slider("option", "value", 5);
 
-getTwoCorrespondingTextsOfSlider = (sliderElement) ->
-    return $(sliderElement).parent().parent().find('.emotionTexts span')
+getTwoCorrespondingPicturesOfSlider = (sliderElement) ->
+    return $(sliderElement).parent().parent().find('.emotionPictures img')
 
-setSizeOfEmotionTexts = (emotionTextElements, sliderValue) ->
-    positiveTextSize = sliderValue
-    negativeTextSize = 28 - sliderValue + 8
-    $(emotionTextElements).filter('.positive').css('font-size', positiveTextSize)
-    $(emotionTextElements).filter('.negative').css('font-size', negativeTextSize)
+setSizeOfEmotionPictures = (emotionPictures, sliderValue) ->
+    firstOpacity = 1 - (sliderValue / 10)
+    secondOpacity = sliderValue / 10
+    $(emotionPictures).filter('.firstEmotion').css('opacity', firstOpacity)
+    $(emotionPictures).filter('.secondEmotion').css('opacity', secondOpacity)
+
+sendEmotionsOnSubmitButtonClick = ->
+    $('#submitFeelings').click(sendEmotions)
+
+sendEmotions = ->
+    console.log("send emotions")
+    emotionStateArray = getEmotionStateArray()
+    sendEmotionStateArray(emotionStateArray)
+
+getEmotionStateArray = ->
+    emotionPictures = $('.oneEmotion .emotionPictures img.firstEmotion')
+    opacityArray = []
+    for emotionPicture in emotionPictures
+        opacityArray.push(parseFloat($(emotionPicture).css('opacity')))
+    console.log(opacityArray)
+    return opacityArray
+
+sendEmotionStateArray = (emotionStateArray) ->
+    $.ajax ({
+            url: 'http://zukunfts-management.de/emotional-social-learning/php/submitEmotionState.php'
+            data: {
+                emotionState: JSON.stringify(emotionStateArray)
+            }
+            dataType: "jsonp"
+        }
+    # jsonp resposne will call emotion state updated with emotion state array :)
+    )
+
+window.emotionStateUpdated = (newEmotionState) ->
+    console.log("received")
+    window.showEmotionState(newEmotionState)
+
+getEmotionStateOfEverybodyAndShowIt = ->
+    $.ajax ({
+            url: 'http://zukunfts-management.de/emotional-social-learning/php/getEmotionState.php'
+            dataType: "jsonp"
+        }
+    ) 
+    # jsonp resposne will call showEmotionState with emotion state array :) 
+    
+window.showEmotionState = (emotionStateObject) ->
+    emotionStateArray = emotionStateObject.emotionStateArray
+    numberOfSubmissions = emotionStateObject.numberOfSubmissions
+    emotionPictures = $('.communityEmotion img')
+    emotionPictures.each( (index, element) ->
+        firstEmotionOpacity = (emotionStateArray[Math.floor(index / 2)] / numberOfSubmissions)
+        if ($(element).is('.firstEmotion'))
+            opacity = firstEmotionOpacity
+        else 
+            opacity = 1 - firstEmotionOpacity
+        $(element).css('opacity', opacity)
+    )
 
 fillAllEmotionContainers()
 addSlidersToEmotionContainers()
-setupSlidersToChangeTextOnChange()
+setupSlidersToChangePicturesOnChange()
+sendEmotionsOnSubmitButtonClick()
+getEmotionStateOfEverybodyAndShowIt()
--- js/fill-emotion-containers.js
+++ js/fill-emotion-containers.js
@@ -1,49 +1,123 @@
 (function() {
-  var addSlidersToEmotionContainers, fillAllEmotionContainers, getTwoCorrespondingTextsOfSlider, setSizeOfEmotionTexts, setupSlidersToChangeTextOnChange;
+  var addSlidersToEmotionContainers, fillAllEmotionContainers, fillCommunityEmotionContainers, fillInvididualEmotionContainers, getEmotionStateArray, getEmotionStateOfEverybodyAndShowIt, getTwoCorrespondingPicturesOfSlider, sendEmotionStateArray, sendEmotions, sendEmotionsOnSubmitButtonClick, setSizeOfEmotionPictures, setupSlidersToChangePicturesOnChange;
 
   console.log($('.emotionContainer'));
 
   fillAllEmotionContainers = function() {
+    fillInvididualEmotionContainers();
+    return fillCommunityEmotionContainers();
+  };
+
+  fillInvididualEmotionContainers = function() {
     var emotionContainerHTML;
-    emotionContainerHTML = "<div class='oneEmotion'>            <div class='emotionTexts'>                <span class='negative'>bored</span>                <span class='positive'>fascinated</span>            </div>        <div class='emotionSlider'></div>        </div>        <div class='oneEmotion'>            <div class='emotionTexts'>                <span class='negative'>overwhelmed</span>                <span class='positive'>unchallenged</span>            </div>            <div class='emotionSlider'></div>        </div>        ";
+    emotionContainerHTML = "<div class='oneEmotion'>            <div class='emotionPictures'>                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/frustrated.jpg' alt='frustated' class='firstEmotion' />                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/unchallenged.jpg' alt='unchallenged' class='secondEmotion' />            </div>        <div class='emotionSlider'></div>        </div>        <div class='oneEmotion'>            <div class='emotionPictures'>                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/bored.jpg' alt='bored' class='firstEmotion' />                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/excited.jpg' alt='excited' class='secondEmotion' />            </div>            <div class='emotionSlider'></div>        </div>        ";
     return $('.emotionContainer').html(emotionContainerHTML);
+  };
+
+  fillCommunityEmotionContainers = function() {
+    var emotionContainerHTML;
+    emotionContainerHTML = "<div class='communityEmotion'>            <div class='emotionPictures'>                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/frustrated.jpg' alt='frustated' class='firstEmotion' />                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/unchallenged.jpg' alt='unchallenged' class='secondEmotion' />            </div>        </div>        <div class='communityEmotion'>            <div class='emotionPictures'>                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/bored.jpg' alt='bored' class='firstEmotion' />                <img src='http://dl.dropbox.com/u/34637013/uni/design-new-learning-environments/images/excited.jpg' alt='excited' class='secondEmotion' />            </div>        </div>        ";
+    return $('.communityEmotionContainer').html(emotionContainerHTML);
   };
 
   addSlidersToEmotionContainers = function() {
     return $('.emotionSlider').slider({
-      min: 8,
-      max: 28
+      min: 0,
+      max: 10
     });
   };
 
-  setupSlidersToChangeTextOnChange = function() {
+  setupSlidersToChangePicturesOnChange = function() {
     $(".emotionSlider").on("slide slidechange", function(event, ui) {
-      var emotionTextElements;
-      emotionTextElements = getTwoCorrespondingTextsOfSlider(ui.handle);
-      console.log("emotext", emotionTextElements);
-      setSizeOfEmotionTexts(emotionTextElements, ui.value);
-      console.log("slider changed!:) to " + ui.value);
-      return console.log(ui.handle);
+      var emotionPictures;
+      emotionPictures = getTwoCorrespondingPicturesOfSlider(ui.handle);
+      return setSizeOfEmotionPictures(emotionPictures, ui.value);
     });
-    return $('.emotionSlider').slider("option", "value", 18);
+    return $('.emotionSlider').slider("option", "value", 5);
   };
 
-  getTwoCorrespondingTextsOfSlider = function(sliderElement) {
-    return $(sliderElement).parent().parent().find('.emotionTexts span');
+  getTwoCorrespondingPicturesOfSlider = function(sliderElement) {
+    return $(sliderElement).parent().parent().find('.emotionPictures img');
   };
 
-  setSizeOfEmotionTexts = function(emotionTextElements, sliderValue) {
-    var negativeTextSize, positiveTextSize;
-    positiveTextSize = sliderValue;
-    negativeTextSize = 28 - sliderValue + 8;
-    $(emotionTextElements).filter('.positive').css('font-size', positiveTextSize);
-    return $(emotionTextElements).filter('.negative').css('font-size', negativeTextSize);
+  setSizeOfEmotionPictures = function(emotionPictures, sliderValue) {
+    var firstOpacity, secondOpacity;
+    firstOpacity = 1 - (sliderValue / 10);
+    secondOpacity = sliderValue / 10;
+    $(emotionPictures).filter('.firstEmotion').css('opacity', firstOpacity);
+    return $(emotionPictures).filter('.secondEmotion').css('opacity', secondOpacity);
+  };
+
+  sendEmotionsOnSubmitButtonClick = function() {
+    return $('#submitFeelings').click(sendEmotions);
+  };
+
+  sendEmotions = function() {
+    var emotionStateArray;
+    console.log("send emotions");
+    emotionStateArray = getEmotionStateArray();
+    return sendEmotionStateArray(emotionStateArray);
+  };
+
+  getEmotionStateArray = function() {
+    var emotionPicture, emotionPictures, opacityArray, _i, _len;
+    emotionPictures = $('.oneEmotion .emotionPictures img.firstEmotion');
+    opacityArray = [];
+    for (_i = 0, _len = emotionPictures.length; _i < _len; _i++) {
+      emotionPicture = emotionPictures[_i];
+      opacityArray.push(parseFloat($(emotionPicture).css('opacity')));
+    }
+    console.log(opacityArray);
+    return opacityArray;
+  };
+
+  sendEmotionStateArray = function(emotionStateArray) {
+    return $.ajax({
+      url: 'http://zukunfts-management.de/emotional-social-learning/php/submitEmotionState.php',
+      data: {
+        emotionState: JSON.stringify(emotionStateArray)
+      },
+      dataType: "jsonp"
+    });
+  };
+
+  window.emotionStateUpdated = function(newEmotionState) {
+    console.log("received");
+    return window.showEmotionState(newEmotionState);
+  };
+
+  getEmotionStateOfEverybodyAndShowIt = function() {
+    return $.ajax({
+      url: 'http://zukunfts-management.de/emotional-social-learning/php/getEmotionState.php',
+      dataType: "jsonp"
+    });
+  };
+
+  window.showEmotionState = function(emotionStateObject) {
+    var emotionPictures, emotionStateArray, numberOfSubmissions;
+    emotionStateArray = emotionStateObject.emotionStateArray;
+    numberOfSubmissions = emotionStateObject.numberOfSubmissions;
+    emotionPictures = $('.communityEmotion img');
+    return emotionPictures.each(function(index, element) {
+      var firstEmotionOpacity, opacity;
+      firstEmotionOpacity = emotionStateArray[Math.floor(index / 2)] / numberOfSubmissions;
+      if ($(element).is('.firstEmotion')) {
+        opacity = firstEmotionOpacity;
+      } else {
+        opacity = 1 - firstEmotionOpacity;
+      }
+      return $(element).css('opacity', opacity);
+    });
   };
 
   fillAllEmotionContainers();
 
   addSlidersToEmotionContainers();
 
-  setupSlidersToChangeTextOnChange();
+  setupSlidersToChangePicturesOnChange();
+
+  sendEmotionsOnSubmitButtonClick();
+
+  getEmotionStateOfEverybodyAndShowIt();
 
 }).call(this);
